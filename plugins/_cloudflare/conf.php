2cc67a23e0b9d143ff59008b3b609799e8b20003
Bhavic <bhavicp@gmail.com>
2019-04-11 14:24:16 +0100

    Fileshare chanegs again

--- plugins/_cloudflare/conf.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-if(empty($pathToExternals['python']))	// May be path already defined?
-{
-	$pathToExternals['python'] = '';// Something like /usr/bin/python. If empty, will be found in PATH.
-}
